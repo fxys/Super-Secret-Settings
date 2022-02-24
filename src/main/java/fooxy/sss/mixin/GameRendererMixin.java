7f5ab9ec9960726b15f52512de5c78faf52b0133
fxys <94676987+fxys@users.noreply.github.com>
2022-02-24 11:51:26 +0000

    Add files via upload
    Updated source

---
+++ src/main/java/fooxy/sss/mixin/GameRendererMixin.java
@@ -0,0 +1,25 @@
+package fooxy.sss.mixin;
+
+import com.mojang.blaze3d.systems.RenderSystem;
+import fooxy.sss.ShaderUtils;
+import net.minecraft.client.render.GameRenderer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(GameRenderer.class)
+public abstract class GameRendererMixin {
+
+    @Inject(method = "render", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/render/GameRenderer;updateWorldIcon()V"))
+    public void render(CallbackInfo ci) {
+        if(ShaderUtils.enabled && ShaderUtils.shader != null) {
+            RenderSystem.disableBlend();
+            RenderSystem.disableDepthTest();
+            RenderSystem.enableTexture();
+            RenderSystem.resetTextureMatrix();
+            ShaderUtils.shader.render(ShaderUtils.client.getTickDelta());
+        }
+    }
+}
+
